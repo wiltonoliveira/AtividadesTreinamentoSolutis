01b5df186ce31fca01e8bd4a2b2514d9e89f4089
Wilton Oliveira <oliveirajrwilton@gmail.com>
2020-12-15 13:02:54 -0300

    CRUDNode implementado

--- 14.12/CRUDNode/server.js
+++ 14.12/CRUDNode/server.js
@@ -1,13 +1,8 @@
 const express = require('express')
 
 const app = express()
-const bodyparser = require('body-parse')
+const bodyparser = require('body-parser')
 app.use(bodyparser.urlencoded({ extended: true}))
-app.listen(3000, function(){
-
-console.log('servidor rodando na porta 3000')
-})
-
 app.get('/', function(req, res){
     res.render('index.ejs');
 });
@@ -15,6 +10,74 @@ app.get('/', function(req, res){
 app.set('view engine', 'ejs')
 
 app.post('/show', function(req, res){
-    console.log('Teste post...')
+    //criar a coleção “data”, que irá armazenar nossos dados
+    db.collection('data').save(req.body, (err, result) => {
+        if (err) return console.log(err)
+     
+        console.log('Salvo no Banco de Dados')
+        res.redirect('/show')
+      })
 });
 
+const ObjectId = require('mongodb').ObjectID
+ 
+const MongoClient = require('mongodb').MongoClient
+const uri = "mongodb+srv://teste:teste@cluster0.bz8ef.mongodb.net/crud?retryWrites=true&w=majority";
+ 
+MongoClient.connect(uri, { useUnifiedTopology:true },(err, client) => {
+  if (err) return console.log(err)
+  db = client.db('crud') // coloque o nome do seu DB
+ 
+  app.listen(3000, () => {
+    console.log('Server running on port 3000')
+  })
+})
+
+app.get('/', (req, res) => {
+    var cursor = db.collection('data').find()
+})
+ 
+app.get('/show', (req, res) => {
+    db.collection('data').find().toArray((err, results) => {
+        if (err) return console.log(err)
+        res.render('show.ejs', { data: results })
+ 
+    })
+})
+
+app.route('/edit/:id')
+.get((req, res) => {
+  var id = req.params.id
+ 
+  db.collection('data').find(ObjectId(id)).toArray((err, result) => {
+    if (err) return res.send(err)
+    res.render('edit.ejs', { data: result })
+  })
+})
+.post((req, res) => {
+  var id = req.params.id
+  var name = req.body.name
+  var surname = req.body.surname
+ 
+  db.collection('data').updateOne({_id: ObjectId(id)}, {
+    $set: {
+      name: name,
+      surname: surname
+    }
+  }, (err, result) => {
+    if (err) return res.send(err)
+    res.redirect('/show')
+    console.log('Atualizado no Banco de Dados')
+  })
+})
+
+app.route('/delete/:id')
+.get((req, res) => {
+  var id = req.params.id
+ 
+  db.collection('data').deleteOne({_id: ObjectId(id)}, (err, result) => {
+    if (err) return res.send(500, err)
+    console.log('Deletado do Banco de Dados!')
+    res.redirect('/show')
+  })
+})
